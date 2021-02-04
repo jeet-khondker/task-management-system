feef9121d8067da85c94884157555cd1738232ef
Jeet Khondker <jeetzhkhondker@gmail.com>
2021-02-04 20:06:51 +0900

    feature: Created the Custom User Account Admin Interface

--- tms_backend/user_app/admin.py
+++ tms_backend/user_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .models import UserAccount
+
+# User Account Admin
+class UserAccountAdmin(BaseUserAdmin):
+    list_display = ("email", "full_name", "date_joined", "last_login", "is_admin", "is_staff",)
+    search_fields = ("email", "full_name",)
+    readonly_fields = ("id", "date_joined", "last_login",)
+    ordering = ("email",)
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(UserAccount, UserAccountAdmin)
