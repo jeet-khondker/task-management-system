95735a82d505846fccdb2f0750ec2d68b601ef4c
Jeet Khondker <jeetzhkhondker@gmail.com>
2021-02-06 16:37:12 +0900

    feature: Created the Task Admin Interface

--- tms_backend/todo_app/admin.py
+++ tms_backend/todo_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task
+
+# Task Admin
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "date_created", "date_updated", "due_date", "is_completed", "date_completed")
+
+admin.site.register(Task, TaskAdmin)
