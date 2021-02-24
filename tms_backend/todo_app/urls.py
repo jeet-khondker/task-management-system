2e2a1a423fc9c37345cc8587186b1e8e83ecee30
Jeet Khondker <jeetzhkhondker@gmail.com>
2021-02-24 18:54:34 +0900

    feature: Created URL Pattern for 'Update Task View'

--- tms_backend/todo_app/urls.py
+++ tms_backend/todo_app/urls.py
@@ -3,11 +3,12 @@
 
 from django.urls import path
 
-from .views import TaskAPIOverview, TaskList, TaskDetail, CreateTask
+from .views import TaskAPIOverview, TaskList, TaskDetail, CreateTask, UpdateTask
 
 urlpatterns = [
     path('', TaskAPIOverview.as_view()),
     path("tasks/", TaskList.as_view()),
     path("tasks/<int:pk>/", TaskDetail.as_view()),
     path("tasks/create/", CreateTask.as_view()),
+    path("tasks/update/<int:pk>/", UpdateTask.as_view()),
 ]
