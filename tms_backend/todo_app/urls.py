c9c74e2a789cdaaf145dd009cecf656034499e26
Jeet Khondker <jeetzhkhondker@gmail.com>
2021-02-24 15:16:03 +0900

    feature: Created URL Pattern for 'Create Task View'

--- tms_backend/todo_app/urls.py
+++ tms_backend/todo_app/urls.py
@@ -3,10 +3,11 @@
 
 from django.urls import path
 
-from .views import TaskAPIOverview, TaskList, TaskDetail
+from .views import TaskAPIOverview, TaskList, TaskDetail, CreateTask
 
 urlpatterns = [
     path('', TaskAPIOverview.as_view()),
     path("tasks/", TaskList.as_view()),
     path("tasks/<int:pk>/", TaskDetail.as_view()),
+    path("tasks/create/", CreateTask.as_view()),
 ]
